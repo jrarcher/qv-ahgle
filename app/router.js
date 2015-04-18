96e2ab81ea3b00dd209c9aafb50a1655922a6446
jrarcher <ryanopolis@gmail.com>
2015-04-18 16:25:01 -0500

    routes and such

--- app/router.js
+++ app/router.js
@@ -2,17 +2,33 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-    location: config.locationType
+	location: config.locationType
 });
 
 Router.map(function() {
-    this.resource('company', function() {
-        this.route('view', {
-            path: '/:id'
-        });
-        this.route('new');
-        this.route('edit', {path:'/:id/edit'});
-    });
+	this.resource('company', function() {
+		this.route('view', {
+			path: '/:id'
+		});
+		this.route('new');
+		this.route('edit', {
+			path: '/:id/edit'
+		});
+	});
+	this.resource('user', function() {
+		this.route('view', path: '/:id'),
+		this.route('new'),
+		this.route('edit', {
+			path: '/:id/edit'
+		});
+	});
+	this.resource('survey', function() {
+		this.route('view', path: '/:id'),
+		this.route('new'),
+		this.route('edit', {
+			path: '/:id/edit'
+		});
+	});
 });
 
 export
